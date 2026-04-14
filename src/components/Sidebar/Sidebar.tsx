5bae0fa7ddc94dc5c4ec4742e10b8d9d4007cfdf
Eduarda Souza da Silva. <esds.snf23@uea.edu.br>
2026-04-14 11:59:04 -0400

    Sidebar has been created

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,5 +1,32 @@
 import "./Sidebar.css";
+import { ChevronLeft, Moon } from "lucide-react";
 
-export function Sidebar() {
-  return <div>Sidebar</div>;
+export function ChatSidebar() {
+  return (
+    <div className="sidebar">
+      <div className="menu-header">
+        <h1 className="menu-title">StanAI</h1>
+
+        <div className="arrow-left">
+          <ChevronLeft></ChevronLeft>
+        </div>
+      </div>
+
+      <div className="new-page-button-container">
+        <button className="new-page-button">Nova Página</button>
+      </div>
+
+      <p className="recents">Recentes:</p>
+
+      <div className="menu">
+        <button className="old-page">babd</button>
+        <button className="old-page">inwew</button>
+        <button className="old-page">niwuqvbr</button>
+      </div>
+
+     <div className="moon-container">
+       <Moon></Moon>
+     </div>
+    </div>
+  );
 }
